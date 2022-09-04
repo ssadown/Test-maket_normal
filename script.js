cb27feb46182aec4bef9750293832b1695e5559b
ssadown <111707454+ssadown@users.noreply.github.com>
2022-09-04 20:14:50 +0500

    Header
    Я люблю программирование!!!

---
+++ script.js
@@ -0,0 +1,10 @@
+let search = document.querySelector('.header_right_input_border');
+let close = document.querySelector('.header_right_btn_off_active');
+function opacity() {
+    search.style.opacity = '1';
+    close.style.opacity = '1';
+};
+function close_opacity() {
+    search.style.opacity = '0';
+    close.style.opacity = '0';
+};
